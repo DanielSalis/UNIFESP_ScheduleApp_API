b3efc3cb3802495a6e007db933aa1428d2ef6418
DanielSalis <danielsalis98@gmail.com>
2021-01-21 18:33:53 -0300

    feat: adiconando conexão com banco e rota de usuários

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+require('./database/index');
 const express = require('express');
 const cors = require('cors');
+
 
 const app = express();
 app.use(cors());
@@ -10,6 +12,7 @@ app.use(express.json({extend: false}));
 //Routes
 //Pasta routes ~> Pasta api ~> arquivo hello
 app.use('/api/hello', require('./routes/api/hello'));
+app.use('/api/user', require('./routes/api/user'))
 
 const PORT = process.env.PORT ? process.env.PORT : 3333;
 
