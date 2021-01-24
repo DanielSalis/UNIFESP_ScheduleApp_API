7bd11f03a0b9fc7653788ce89efc64a6092328cf
DanielSalis <danielsalis98@gmail.com>
2021-01-23 23:38:27 -0300

    feat:  adicionando rota para deletar id

--- routes/api/user.js
+++ routes/api/user.js
@@ -15,10 +15,10 @@ router.get('/getById/:id', async(request,response)=>{
   catch(error){
     return response.sendStatus(500);
   }
-})
+});
 
 // @route  POST api/users/getById/:id
-// @desc   Get all users
+// @desc   Get user by id
 // @acess  Public
 router.get('/getAll', async(request,response)=>{
   try{
@@ -28,23 +28,42 @@ router.get('/getAll', async(request,response)=>{
   catch(error){
     return response.sendStatus(500);
   }
-})
-
+});
 
 // @route  POST api/users/create
 // @desc   Create User
 // @acess  Public
 router.post('/create', async(request, response) =>{
-    const {name, last_name, email, password, profile_id, schedule_type_id, vacation_type_id, department_id} = request.body;
+  const {name, last_name, email, password, profile_id, schedule_type_id, vacation_type_id, department_id} = request.body;
 
-    try{
-        const user = await User.create({ name, last_name, email, password, profile_id, schedule_type_id, vacation_type_id, department_id});
-        return response.json(user);
-    }
-    catch (error){
-        console.log(error);
-        return response.sendStatus(500);
-    }
+  try{
+      const user = await User.create({ name, last_name, email, password, profile_id, schedule_type_id, vacation_type_id, department_id});
+      return response.json(user);
+  }
+  catch (error){
+      console.log(error);
+      return response.sendStatus(500);
+  }
+});
+
+// @route  POST api/users/deleteById
+// @desc   Delete User
+// @acess  Public
+router.post('/deleteById', async(request, response) =>{
+  const {id} = request.body;
+
+  try{
+      const user = await User.destroy({
+        where:{
+          id:id
+        }
+      })
+      return response.json({'Message': 'Usuário excluido com sucesso!',user: user});
+  }
+  catch (error){
+      console.log(error);
+      return response.sendStatus(500);
+  }
 });
 
 module.exports = router;
